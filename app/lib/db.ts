943b9ac736fd5c3e408ced5e09ae1b9da7a889cd
NEELAMSETTI YOGIRAJ <yogineelamsetti@gmail.com>
2025-06-01 19:43:41 +0530

    db changes again

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,4 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prismaClient = new PrismaClient().$extends(withAccelerate());
+export const prismaClient = new PrismaClient();
